0ab980ede16f11b621c5551a7899794972b6a9ed
lsnellin <logansnelling730@gmail.com>
2023-11-05 02:03:18 -0500

    Fixed import

--- backend/textGen/urls.py
+++ backend/textGen/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CourseListCreateView, TestCreateView
+from .views import CourseListCreateView
 
 urlpatterns = [
     path('', CourseListCreateView.as_view(), name="course-list-create")
